89b538e970980e0d317f261304bb99c4c8f4b102
Delph <delphic.uk@gmail.com>
2022-05-05 08:38:55 +0100

    Added createTextureArray method to Texture module

--- src/texture.js
+++ src/texture.js
@@ -8,8 +8,11 @@ module.exports = (function(){
     exports.create = (config) => {
         let { source, quality = TextureQuality.Low, clamp = false, flipY = true, disableAnsio = false } = config;
 
+        // HACK: disableAnsio exists only due to the lack of ability to configure
+        // texture filtering and should be removed once this capability is added
+
         if (!source) {
-            console.error("No source provided to Texture.create");
+            console.error("Null source provided to Texture.create config");
             return null;
         }
 
@@ -17,6 +20,17 @@ module.exports = (function(){
         // Arguably should extract concept of texture quality from Renderer and just pass min max filters,
         // generateMipMaps, & enableAnsio
         return Renderer.createTexture(source, quality, clamp, flipY, disableAnsio);
+    };
+
+    exports.createTextureArray = (config) => {
+        let { source, width, height, imageCount, quality = TextureQuality.Low, clamp = false } = config;
+
+        if (!source || !width || !height || !imageCount) {
+            console.error("Texture array config requires source, width, height and imageCount, provided " + JSON.stringify(config));
+            return null;
+        }
+
+        return Renderer.createTextureArray(source, width, height, imageCount, quality, clamp);
     };
 
     return exports;
